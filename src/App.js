b58ee3d5ff1e578b5a989aa851da630f39899a60
Florin <florin@dnvn.co>
2020-06-27 08:35:16 -0700

    Basic scaffold, configs, and simple css resets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./styles.css";
+// ... Import api fetch/transform helper
+// ... Import components
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    /* 
+      Leaving out semantic tags like main/header/footer
+      since this as a component context rather than a 
+      self standing web app.
+    */
+
+    <div className="ImageSearch">
+      {/* ... add SearchForm component */}
+      {/* ... add SearchResults component */}
+      {/* ... add SavedImages component */}
     </div>
   );
 }
